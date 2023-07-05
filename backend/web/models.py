937b60c9887893b0e0576eb69452b261128c8232
nopeless <38830903+nopeless@users.noreply.github.com>
2023-07-05 20:09:54 +0900

    fix model

--- backend/web/models.py
+++ backend/web/models.py
@@ -1,17 +1,21 @@
 from django.db import models
 
 
-class U(models.Model):
-    user = models.ForeignKey(User, on_delete = models.DO_NOTHING)
-    is_subscribed = models.BooleanField()
-    
+class User(models.Model):
+    email = models.EmailField(primary_key=True)
+    is_subscribed = models.BigIntegerField(default=0)
+
+    def is_subscribed_to_channel(self, channel):
+        return self.is_subscribed & channel != 0
+
+
 # Create your models here.
 class Article(models.Model):
     article_id = models.UUIDField()
     title = models.TextField()
     author = models.TextField()
     date_published = models.DateTimeField()
-    #image = models.ImageField(null=True)
+    # image = models.ImageField(null=True)
     textual_content = models.TextField()
     description = models.TextField(null=True)
 
